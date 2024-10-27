33e79d1053236350a646e52634745c40326131f1
oskarcokl <oskar.cokl@gmail.com>
2024-10-27 22:05:04 +0100

    feat: Add simple register form.

--- chakra/src/components/Login.tsx
+++ chakra/src/components/Login.tsx
@@ -31,5 +31,5 @@ export default function Login() {
                 </form>
             </Card.Body>
         </Card.Root>
-   )
+    )
 }
--- chakra/src/components/Register.tsx
+++ chakra/src/components/Register.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React from "react"
+import { Stack, Input, Button, Card } from "@chakra-ui/react"
+import { Field } from "./ui/field"
+import { PasswordInput } from "./ui/password-input"
 
 export default function Register() {
+    function loginHandler(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formElements = Array.from((e.target as HTMLFormElement).elements) as HTMLFormElement[];
+        const formValues = formElements.map(formElement => formElement.value);
+        console.log(formValues)
+    }
+
     return (
-        <h1>Register</h1>
+        <Card.Root w="sm">
+            <Card.Header>
+                <Card.Title>Register</Card.Title>
+                <Card.Description>Fill out the information bellow to create an account.</Card.Description>
+            </Card.Header>
+            <Card.Body>
+                <form onSubmit={loginHandler}>
+                    <Stack gap="4" align="flex-end" maxW="sm">
+                        <Field label="First Name">
+                            <Input name="first-name" placeholder="Enter your first name"/>
+                        </Field>
+                        <Field label="Last Name">
+                            <Input name="last-name" placeholder="Enter your last name"/>
+                        </Field>
+                        <Field label="Email">
+                            <Input name="email" placeholder="Enter your email"/>
+                        </Field>
+                        <Field label="Password">
+                            <PasswordInput name="password" placeholder="Enter your password"/>
+                        </Field>
+                        <Field label="Repeat Password">
+                            <PasswordInput name="re-password" placeholder="Enter your password again :)"/>
+                        </Field>
+                        <Button type="submit">Register</Button>
+                    </Stack>
+                </form>
+            </Card.Body>
+        </Card.Root>
     )
 }
