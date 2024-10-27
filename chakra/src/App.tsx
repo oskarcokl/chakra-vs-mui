e48ad8a5d6de5ae062680659357e4c1af9e61245
oskarcokl <oskar.cokl@gmail.com>
2024-10-27 21:12:57 +0100

    feat: Using chakra list for navbar.

--- chakra/src/App.tsx
+++ chakra/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
+import { List } from "@chakra-ui/react"
 import { Provider } from "./components/ui/provider";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -19,18 +20,22 @@ function App() {
 }
 
 function NavLinks() {
+    // NOTE: Might be a better way of doing this but I honestly have no idea
+    // what I'm doing.
     const location = useLocation();
     const isRoot = location.pathname === "/"
     
     return <>
-        { isRoot && (<ul>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-            <li>
-                <Link to="/register">Register</Link>
-            </li>
-        </ul>)}
+        { isRoot && (
+            <List.Root>
+                <List.Item>
+                    <Link to="/login">Login</Link>
+                </List.Item>
+                <List.Item>
+                    <Link to="/register">Register</Link>
+                </List.Item>
+            </List.Root>
+        )}
     </>
 }
 
