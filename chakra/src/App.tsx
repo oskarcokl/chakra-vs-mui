38908e01cb43fa7e0a3254b7d45ae96bea989b78
oskarcokl <oskar.cokl@gmail.com>
2024-10-27 20:51:07 +0100

    feat: Add basic navigation with react router.

--- chakra/src/App.tsx
+++ chakra/src/App.tsx
@@ -1,15 +1,37 @@
 import React from "react"
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import { Provider } from "./components/ui/provider";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
     return (
-        <Provider>
-            <Login/>
-            <Register/>
-        </Provider>
+        <BrowserRouter>
+            <Provider>
+                <NavLinks/>
+                <Routes>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="register" element={<Register/>}/>
+                </Routes>
+            </Provider>
+        </BrowserRouter>
     );
+}
+
+function NavLinks() {
+    const location = useLocation();
+    const isRoot = location.pathname === "/"
+    
+    return <>
+        { isRoot && (<ul>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+        </ul>)}
+    </>
 }
 
 export default App;
