76eebe21cf4d74c13958b018c4f627700ceed37c
oskarcokl <oskar.cokl@gmail.com>
2024-11-19 22:03:59 +0100

    feat: Submiting Expense form data.

--- mui/src/components/ExpenseInputForm.tsx
+++ mui/src/components/ExpenseInputForm.tsx
@@ -1,5 +1,20 @@
-import { Card, CardHeader, CardContent, Stack, TextField, Select, MenuItem, InputLabel, FormControl, Button } from '@mui/material';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    Stack,
+    TextField,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+    Button,
+    FormHelperText,
+} from '@mui/material';
 import { NumericFormat } from 'react-number-format';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 const categories = [
     'Food & Dining',
@@ -8,30 +23,52 @@ const categories = [
     'Bills & Utilities',
     'Shopping',
     'Healthcare',
-    'Other'
+    'Other',
 ];
 
-const ExpenseInputForm = () => {
+const expenseSchema = z.object({
+    description: z.string().min(1, 'Description is required'),
+    category: z.string().min(1, 'You must choose a category'),
+    amount: z.string().min(1, 'Amount is required'),
+    date: z.string().date().min(1, 'Date is required'),
+});
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log('Form submitted');
-    }
+type ExpenseFormData = z.infer<typeof expenseSchema>;
+
+const ExpenseInputForm = () => {
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm<ExpenseFormData>({
+        resolver: zodResolver(expenseSchema),
+    });
+
+    const submitExpense: SubmitHandler<ExpenseFormData> = (data) => {
+        console.log('Form submitted', data);
+    };
 
     return (
         <Card variant="outlined" sx={{ maxWidth: '400px', margin: 'auto', mt: 10 }}>
             <CardHeader title="Expense Input Form" />
             <CardContent>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(submitExpense)}>
                     <Stack spacing={2}>
-                        <TextField label="Description" />
+                        <TextField
+                            label="Description"
+                            {...register('description')}
+                            error={!!errors.description}
+                            helperText={errors.description?.message}
+                        />
                         <FormControl>
                             <InputLabel id="category-label">Category</InputLabel>
-                            <Select label="Category" labelId="category-label">
+                            <Select
+                                label="Category"
+                                labelId="category-label"
+                                {...register('category')}
+                                error={!!errors.category}
+                            >
                                 {categories.map((category) => (
-                                <MenuItem value={category}>{category}</MenuItem>
+                                    <MenuItem value={category}>{category}</MenuItem>
                                 ))}
                             </Select>
+                            <FormHelperText error={!!errors.category}>{errors.category?.message}</FormHelperText>
                         </FormControl>
                         <NumericFormat
                             customInput={TextField}
@@ -42,19 +79,28 @@ const ExpenseInputForm = () => {
                             decimalScale={2}
                             allowNegative={false}
                             valueIsNumericString
+                            error={!!errors.amount}
+                            helperText={errors.amount?.message}
+                            onValueChange={(values) => {
+                                setValue('amount', values.value);
+                            }}
                         />
                         <TextField
                             label="Date"
                             type="date"
                             defaultValue={new Date().toISOString().split('T')[0]}
+                            {...register('date')}
+                            error={!!errors.date}
+                            helperText={errors.date?.message}
                         />
-                        <Button type="submit" variant="contained" color="primary">Add Expense</Button>
+                        <Button type="submit" variant="contained" color="primary">
+                            Add Expense
+                        </Button>
                     </Stack>
-
                 </form>
             </CardContent>
         </Card>
-    )
-}
+    );
+};
 
-export default ExpenseInputForm
+export default ExpenseInputForm;
